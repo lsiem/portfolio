615a2f4a0b0ba725a4b2a0d06a2e81386f9c2b68
Lasse Siemoneit <lasse.siemoneit@gmail.com>
2024-05-02 18:07:22 +0200

    Changes to MainProjectCard

--- src/components/mainProjectCard/MainProjectCard.js
+++ src/components/mainProjectCard/MainProjectCard.js
@@ -5,11 +5,8 @@ import ProjectLanguages from "../projectLanguages/ProjectLanguages";
 const MainProjectCard = ({ project, theme }) => {
   return (
     <div className="main-project-card">
-      <div className="main-project-title">
-        <h2>{project.title}</h2>
-      </div>
       <div className="main-project-details">
-        <h3>{project.title}</h3>
+        <h2 className="main-project-title">{project.title}</h2>
         <p className="main-project-description">{project.description}</p>
         <div className="main-project-footer">
           <ProjectLanguages logos={project.technologies} />
