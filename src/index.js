8c4a72d3e15e29831884b1423f47350667c8e4e1
Lasse Siemoneit <lasse.siemoneit@gmail.com>
2024-05-07 03:20:35 +0200

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const engine = new Styletron();
 
@@ -19,8 +18,3 @@ ReactDOM.render(
   </StyletronProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
